5fd35bead65f76ce5836917734af6bd0dbdba0a2
Kartik <pokemonemerald0@gmail.com>
2016-11-16 16:11:21 +0530

    Upload

---
+++ include/libs.h
@@ -0,0 +1,25 @@
+#ifndef LIB_FILE
+
+#define LIB_FILE
+#include <3ds.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include "offset.h"
+#include "conversion.h"
+#include "crc.h"
+#include "fs.h"
+extern char loca[1024];
+#endif
